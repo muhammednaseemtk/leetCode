f179c749e6e010f0fa7b730a98a014e0cc5b3fd8
muhammed naseem t k <muhammednaseemtk000@gmail.com>
2025-09-14 20:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 152.6 MB (5.66%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.dart
@@ -0,0 +1,15 @@
+class Solution {
+  int lengthOfLastWord(String s) {
+    int length = 0;
+    int i = s.length - 1;
+
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+    return length;
+  }
+}
