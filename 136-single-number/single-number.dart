e52a6929cb00db75fba1ff93a46362756ebd36bb
muhammed naseem t k <muhammednaseemtk000@gmail.com>
2025-09-17 20:52:12 +0530

    Time: 38 ms (20.00%) | Memory: 150.9 MB (56.55%) - LeetSync

---
+++ 136-single-number/single-number.dart
@@ -0,0 +1,13 @@
+class Solution {
+  int singleNumber(List<int> nums) {
+    Set<int> set = Set();
+    for(int num in nums){
+        if(set.contains(num)){
+            set.remove(num);
+        }else{
+            set.add(num);
+        }
+    }
+    return set.first;
+  }
+}
