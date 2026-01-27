67b8580cbd8bc39064b0a81bfd2f19de9ab3c7a2
muhammed naseem t k <muhammednaseemtk000@gmail.com>
2026-01-27 19:04:53 +0530

    Time: 28 ms (14.53%) | Memory: 151.2 MB (75.19%) - LeetSync

--- 9-palindrome-number/palindrome-number.dart
+++ 9-palindrome-number/palindrome-number.dart
@@ -1,7 +1,7 @@
 class Solution {
   bool isPalindrome(int x) {
-    String name = x.toString();
-    String reverse = name.split('').reversed.join('');
-    return name == reverse;
+    String s = x.toString();
+    String n = s.split('').reversed.join();
+    return s == n;
   }
 }
