a46b5562c25b873465e9715a3f5424f804fa7e71
muhammed naseem t k <muhammednaseemtk000@gmail.com>
2026-04-04 09:56:38 +0530

    Time: 0 ms (100%), Space: 148.3 MB (75%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.dart
@@ -0,0 +1,15 @@
+class Solution {
+  int countPairs(List<int> nums, int target) {
+    int count = 0;
+    int n = nums.length;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(nums[i] + nums[j] < target) {
+                count++;
+            }
+        }
+    }
+    return count;
+  }
+}
