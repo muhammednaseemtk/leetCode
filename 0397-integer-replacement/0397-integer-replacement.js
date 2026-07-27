32d6a349364f7b71b63170b9c55d8a665490719a
muhammed naseem t k <muhammednaseemtk000@gmail.com>
2026-07-27 20:14:15 +0530

    Time: 0 ms (100%), Space: 52.8 MB (95.56%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.js
@@ -0,0 +1,16 @@
+var integerReplacement = function(n) {
+    let count = 0;
+    while (n !== 1) {
+        if ((n & 1) === 0) {
+            n >>>= 1;
+        } else {
+            if (n === 3 || ((n >> 1) & 1) === 0) {
+                n--;
+            } else {
+                n++;
+            }
+        }
+        count++;
+    }
+    return count;
+};
